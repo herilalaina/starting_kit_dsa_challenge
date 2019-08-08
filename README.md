21e39652f31b683daa772dc1d4c9502e165987be
herilalaina <rkt.herilalaina@gmail.com>
2019-08-08 20:27:44 +0200

    add html page

--- README.md
+++ README.md
@@ -1,19 +1,26 @@
-This is a sample starting kit for the Iris challenge. 
-It uses the well known Iris dataset from Fisher's classic paper (Fisher, 1936). The data set contains 3 classes of 50 instances each, where each class refers to a type of iris plant. One class is linearly separable from the other 2; the latter are NOT linearly separable from each other.
-
-References and credits: 
-R. A. Fisher. The use of multiple measurements in taxonomic problems. Annual Eugenics, 7, Part II, 179-188 (1936). 
+This is a sample starting kit for the Data Science Africa challenge.
+It uses the Microscopy dataset from http://air.ug/microscopy/.
 
 Prerequisites:
-Install Anaconda Python 3.6.6 
+Install Anaconda Python 3.6.6, Tensorflow (2.0.0), opencv-python (4.0.1), scikit-image (0.15.0)
+
+or
+
+run your code within the Codalab docker (inside the docker, python 3.6 is called python3):
+
+	`docker run -it -p 8888:8888 -v `pwd`:/home/aux herilalaina/dsa:2.0`
+
+	`DockerPrompt# cd /home/aux`
+	`DockerPrompt# python3 ingestion_program/ingestion.py sample_data sample_result_submission ingestion_program sample_code_submission`
+	`DockerPrompt# python3 scoring_program/score.py sample_data sample_result_submission scoring_output`
+	`DockerPrompt# exit`
+
+
 
 Usage:
+- The two files sample_*_submission.zip are sample submissions ready to go!
 
-(1) If you are a challenge participant:
-
-- The three files sample_*_submission.zip are sample submissions ready to go!
-
-- The file README.ipynb contains step-by-step instructions on how to create a sample submission for the Iris challenge. 
+- The file README.ipynb contains step-by-step instructions on how to create a sample submission for the challenge.
 At the prompt type:
 jupyter-notebook README.ipynb
 
@@ -27,25 +34,3 @@ jupyter-notebook README.ipynb
 
 then zip the contents of sample_result_submission (without the directory).
 
-(2) If you are a challenge organizer and use this starting kit as a template, ensure that:
-
-- you modify README.ipynb to provide a good introduction to the problem and good data visualization
-
-- sample_data is a small data subset carved out the challenge TRAINING data, for practice purposes only (do not compromise real validation or test data)
-
-- the following programs run properly:
-
-    `python ingestion_program/ingestion.py sample_data sample_result_submission ingestion_program sample_code_submission`
-
-    `python scoring_program/score.py sample_data sample_result_submission scoring_output`
-
-- the metric identified by metric.txt in the utilities directory is the metric used both to compute performances in README.ipynb and for the challenge.
-
-- your code also runs within the Codalab docker (inside the docker, python 3.6 is called python3):
-
-	`docker run -it -v `pwd`:/home/aux codalab/codalab-legacy:py3`
-	
-	`DockerPrompt# cd /home/aux`
-	`DockerPrompt# python3 ingestion_program/ingestion.py sample_data sample_result_submission ingestion_program sample_code_submission`
-	`DockerPrompt# python3 scoring_program/score.py sample_data sample_result_submission scoring_output`
-	`DockerPrompt# exit`
--- sample_code_submission/model.py
+++ sample_code_submission/model.py
@@ -1,81 +1,80 @@
-'''
+"""This file is for patches classification (step 1).
+
 Sample predictive model.
 You must supply at least 4 methods:
 - fit: trains the model.
 - predict: uses the model to perform predictions.
 - save: saves the model.
 - load: reloads the model.
-'''
-import pickle
+"""
 import numpy as np   # We recommend to use numpy arrays
-from os.path import isfile
 from sklearn.base import BaseEstimator
 
-class model (BaseEstimator):
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.layers import Activation, Dropout, Flatten, Dense
+
+
+class model(BaseEstimator):
+    """Main class for Classification problem."""
+
     def __init__(self):
-        '''
-        This constructor is supposed to initialize data members.
-        Use triple quotes for function documentation.
-        '''
-        self.num_train_samples=0
-        self.num_feat=1
-        self.num_labels=1
-        self.is_trained=False
+        """Init method.
+
+        We define here a simple (shallow) CNN.
+        """
+        self.num_train_samples = 0
+        self.num_feat = 1
+        self.num_labels = 1
+        self.is_trained = False
+
+        self.model = Sequential()
+        self.model.add(Conv2D(32, (3, 3), input_shape=(40, 40, 3)))
+        self.model.add(Activation('relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        self.model.add(Flatten())
+        self.model.add(Dense(64))
+
+        self.model.add(Activation('relu'))
+        self.model.add(Dropout(0.5))
+        self.model.add(Dense(1))
+        self.model.add(Activation('sigmoid'))
+
+        self.model.compile(loss='binary_crossentropy', optimizer='rmsprop',
+                           metrics=['accuracy'])
 
     def fit(self, X, y):
-        '''
+        """Fit method.
+
         This function should train the model parameters.
-        Here we do nothing in this example...
         Args:
             X: Training data matrix of dim num_train_samples * num_feat.
-            y: Training label matrix of dim num_train_samples * num_labels.
+               An image has the following shape (40, 40, 3) then 4800 features.
+            y: Training label matrix of dim num_train_samples.
         Both inputs are numpy arrays.
-        For classification, labels could be either numbers 0, 1, ... c-1 for c classe
-        or one-hot encoded vector of zeros, with a 1 at the kth position for class k.
-        The AutoML format support on-hot encoding, which also works for multi-labels problems.
-        Use data_converter.convert_to_num() to convert to the category number format.
-        For regression, labels are continuous values.
-        '''
+        """
         self.num_train_samples = X.shape[0]
-        if X.ndim>1: self.num_feat = X.shape[1]
-        print("FIT: dim(X)= [{:d}, {:d}]".format(self.num_train_samples, self.num_feat))
-        num_train_samples = y.shape[0]
-        if y.ndim>1: self.num_labels = y.shape[1]
-        print("FIT: dim(y)= [{:d}, {:d}]".format(num_train_samples, self.num_labels))
-        if (self.num_train_samples != num_train_samples):
-            print("ARRGH: number of samples in X and y do not match!")
-        self.is_trained=True
+        X = X.reshape((self.num_train_samples, 40, 40, 3))
+        self.model.fit(X / 255.0, y, epochs=5)
+        self.is_trained = True
 
     def predict(self, X):
-        '''
+        """Predict method.
+
+        Args:
+            X: Training data matrix of dim num_train_samples * num_feat.
+               An image has the following shape (40, 40, 3) then 4800 features.
         This function should provide predictions of labels on (test) data.
-        Here we just return zeros...
-        Make sure that the predicted values are in the correct format for the scoring
-        metric. For example, binary classification problems often expect predictions
-        in the form of a discriminant value (if the area under the ROC curve it the metric)
-        rather that predictions of the class labels themselves. For multi-class or multi-labels
-        problems, class probabilities are often expected if the metric is cross-entropy.
+        Make sure that the predicted values are in the correct format for the
+        scoring metric. For example, binary classification problems often
+        expect predictions in the form of a discriminant value (if the area
+        under the ROC curve it the metric) rather that predictions of the class
+        labels themselves. For multi-class or multi-labels problems, class
+        probabilities are often expected if the metric is cross-entropy.
         Scikit-learn also has a function predict-proba, we do not require it.
         The function predict eventually can return probabilities.
-        '''
+        """
         num_test_samples = X.shape[0]
-        if X.ndim>1: num_feat = X.shape[1]
-        print("PREDICT: dim(X)= [{:d}, {:d}]".format(num_test_samples, num_feat))
-        if (self.num_feat != num_feat):
-            print("ARRGH: number of features in X does not match training data!")
-        print("PREDICT: dim(y)= [{:d}, {:d}]".format(num_test_samples, self.num_labels))
-        y = np.zeros([num_test_samples, self.num_labels])
-        # If you uncomment the next line, you get pretty good results for the Iris data :-)
-        #y = np.round(X[:,3])
-        return y
-
-    def save(self, path="./"):
-        pickle.dump(self, open(path + '_model.pickle', "wb"))
-
-    def load(self, path="./"):
-        modelfile = path + '_model.pickle'
-        if isfile(modelfile):
-            with open(modelfile, 'rb') as f:
-                self = pickle.load(f)
-            print("Model reloaded from: " + modelfile)
-        return self
+        X = X.reshape((num_test_samples, 40, 40, 3))
+        return self.model.predict_proba(X / 255.0)
